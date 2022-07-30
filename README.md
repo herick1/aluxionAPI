9760384073d4ec76e9228a5adbef3abebbf30520
herick1 <herick200@gmail.com>
2022-07-30 08:54:31 -0400

    ADD: commit inicial con primer login pero con errores en las vistas html

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aluxionAPI
 proyecto para la prueba tecnica de Node.JS 
+
+# Como instalar el proyecto: 
+npm install
+
+# Ejecutar el proyecto: 
+npm start
+
+
+# El proyecto es acessible directamente a traves de heroku con el enalce:
+https://aluxionproject.herokuapp.com/
+
---
+++ controller/sendMail.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+var smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_ACCOUNT,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+module.exports.sendResetEmail = async (email, token) => {
+  var url = process.env.DOMAIN_NAME + "/user/reset-password?token=" + token;
+
+  await smtpTransport.sendMail({
+    from: process.env.MAIL_ACCOUNT,
+    to: email,
+    subject: "RESET YOUR PASSWORD",
+    text: `Click on this link to reset your password ${url}`,
+    html: `<h3> Click on this link to reset your password : ${url} </h3>`,
+  });
+};
+
+module.exports.sendVerifyEmail = async (email, token) => {
+  // change first part to your domain
+  var url = process.env.DOMAIN_NAME + "/user/verifyemail?token=" + token;
+
+  await smtpTransport.sendMail({
+    from: process.env.MAIL_ACCOUNT,
+    to: email,
+    subject: "VERIFY Your EMAIL",
+    text: `Click on this link to verify ${url}`,
+    html: `<h3> Click on this link to verify your email : ${url} </h3>`,
+  });
+};
